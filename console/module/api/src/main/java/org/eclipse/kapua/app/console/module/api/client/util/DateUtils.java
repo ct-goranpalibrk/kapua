0bba95255dc6fe946dd138fc563c9bda51953a02
CT\pgoran <goran.palibrk@comtrade.com>
2018-04-10 11:35:46 +0200

    timeStamp time and dateRange time not same
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>

--- console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/util/DateUtils.java
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/util/DateUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -35,7 +35,7 @@ public class DateUtils {
         }
 
         TimeZone utcTimeZione = TimeZone.createTimeZone(0);
-        return DateTimeFormat.getFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(d, utcTimeZione);
+        return DateTimeFormat.getFormat("dd/MM/yyyy'T'HH:mm:ss.SSS'Z'").format(d, utcTimeZione);
     }
 
     public static int getYear(Date date) {
